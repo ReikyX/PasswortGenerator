69b4b06f6fda4309247f110dcd4694149d2846d2
Fries <erik.fries@training.lug-ag.de>
2025-02-10 13:14:44 +0100

    implementierung master-pass using Pbkdf2 key derivation unsing SHA-256 Algorithm with salt

--- cerberus-pass/Program.cs
+++ cerberus-pass/Program.cs
@@ -1,25 +1,91 @@
 ﻿// Main UI-Flow
+using System.Security.Cryptography;
 using cerberus_pass;
 
 var manager = new PasswordManager();
 
 Console.ForegroundColor = ConsoleColor.DarkRed;
-Console.WriteLine("\nWillkommen zu Cerberus-Pass! \n");
+Console.WriteLine("Willkommen zu Cerberus-Pass!");
 Console.ResetColor();
+
+// first start:
+// check if file "masterpass.txt" exists
+const string masterPassFilePath = "masterpass.txt";
+if (!File.Exists(masterPassFilePath)) // first start
+{
+    // does not exist
+    // prompt user for new masterpass
+    Console.WriteLine("Um deinen Passwort-Vault einzurichten, gebe ein Master-Passwort ein.");
+    Console.WriteLine("Dieses Passwort wird zur verschlüsselung aller deiner anderen Passwörter verwendet. Stelle sicher, dass dein Passwort komplex und möglichst lang ist, aber auch einfach einzutippen und merken.");
+    Console.WriteLine("Falls du dieses Passwort vergisst, kommst du nicht mehr an deine gespeicherten Passwörter!");
+    var userInput = Console.ReadLine();
+    //   -> not emtpy string
+    if (String.IsNullOrEmpty(userInput))
+    {
+        Console.WriteLine("Master-Passwort muss gesetzt sein!");
+        Environment.Exit(1);
+    }
+    // confirm input
+    Console.WriteLine("Master-Passwort bestätigen:");
+    var userInputConfirm = Console.ReadLine();
+    if (userInput != userInputConfirm)
+    {
+        Console.WriteLine("Eingegebene Passwörter stimmen nicht überein!");
+        Environment.Exit(2);
+    }
+    // hash masterpass
+    var salt = String.Empty;
+    var hashedPassword = HashPassword(userInput, out salt); // salt generieren -> neuer salt
+                                                            // create "masterpass.txt" and write hashed masterpass to it
+                                                            // File.Create(masterPassFilePath).Dispose();
+    File.WriteAllLines(masterPassFilePath,
+    new[] { hashedPassword, salt });
+}
+else // every other start
+{// "masterpass.txt" exists:
+ // prompt user for master-password
+    Console.WriteLine("Gebe dein Master-Passwort ein:");
+    var userInput = Console.ReadLine();
+    // read set masterpass from file
+    var storedMasterPass = File.ReadAllLines(masterPassFilePath);
+    //   -> not empty string -> delete file, end program
+    var storedHash = storedMasterPass[0];
+    var storedSalt = storedMasterPass[1];
+    //  and compare hashed masterpass from file with hashed user input
+    // hash userinput
+    // ursprünglichen salt verwenden
+    if (
+      VerifyPassword(userInput, storedHash, storedSalt)
+    )
+    {
+        Console.WriteLine("Master-Passwort korrekt! Anmeldung erfolgt...");
+        Thread.Sleep(2000);
+        Console.Clear();
+    }
+    else
+    {
+        Console.WriteLine("Passwort stimmt nicht überein. Programm wird beendet!");
+        Environment.Exit(3);
+    }
+    // if no match -> end program with some error
+}
 
 do
 {
     Console.WriteLine("Wähle was du tun willst:");
-    Console.WriteLine(@"
+
+    Console.WriteLine("""
   1. Passwort-Liste ausgeben
   2. Passwort mit ID ausgeben
   3. Neues Passwort erstellen
   4. Vorhandenes Passwort bearbeiten
   5. Passwort löschen
-");
+""");
 
     var userInput = Console.ReadLine();
-    int menuChoice = 0;
+
+    int menuChoice;
+
     if (!int.TryParse(userInput, out menuChoice))
         continue;
 
@@ -34,7 +100,6 @@ do
             break;
         case MenuOptions.GetOne:
             // todo: Exception wenn title nicht existiert
-            // Ein Passwort vollständig ausgeben
             Console.WriteLine("Welchen Eintrag willst du ansehen? (Title):");
             var titleToPrint = Console.ReadLine();
             var entry = manager.GetEntry(titleToPrint);
@@ -50,7 +115,7 @@ do
             var newEntry = manager.CreateEntry(title, login, password);
             if (newEntry is null)
             {
-                Console.WriteLine($"Eintrag mit {title} existiert bereits. Wolltest du diesen Update? Oder erstelle einen neuen mit einem anderen Titel.");
+                Console.WriteLine($"Eintrag mit {title} existiert bereits.\nWolltest du diesen Updaten? Oder erstelle einen neuen mit einem anderen Titel.");
             }
             else
             {
@@ -71,12 +136,11 @@ do
               "Gebe ein neues Passwort für den Eintrag an (Leer um nichts zu ändern):");
             var new_password = Console.ReadLine();
             var oldEntry = manager.GetEntry(title_to_change);
-            var updatedEntry = manager.UpdateEntry
-            (title_to_change, new PasswordEntry(
+            var updatedEntry = manager.UpdateEntry(title_to_change, new PasswordEntry(
               String.IsNullOrEmpty(new_title) ? oldEntry.Title : new_title,
               String.IsNullOrEmpty(new_login) ? oldEntry.Login : new_login,
-              String.IsNullOrEmpty(new_password) ? oldEntry.Password : new_password)
-            );
+              String.IsNullOrEmpty(new_password) ? oldEntry.Password : new_password
+            ));
             Console.WriteLine($"Eintrag {updatedEntry.Title} wurde erfolgreich aktuallisiert.");
             break;
         case MenuOptions.Delete:
@@ -91,9 +155,42 @@ do
         default:
             // Fehler anzeigen -> Eingabe-Hint (1-5)
             // Eingabe wiederholen
-            Console.WriteLine("Falsche Eingabe! Valide Optionen => 1 - 5");
+            Console.WriteLine("Falsche Eingabe! Valide Optionen => 1-5");
             break;
     }
     Console.ReadKey();
     Console.Clear();
 } while (true);
+
+string HashPassword(string password, out string salt)
+{
+    byte[] saltBytes = new byte[16];
+    using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+    {
+        rng.GetBytes(saltBytes);
+    }
+    salt = Convert.ToBase64String(saltBytes);
+    var pbkdf2Bytes = Rfc2898DeriveBytes.Pbkdf2(
+      password,
+      saltBytes,
+      10_000,
+      HashAlgorithmName.SHA256,
+      32
+    );
+    return Convert.ToBase64String(pbkdf2Bytes);
+}
+
+bool VerifyPassword(string enteredPassword, string hashedPassword, string salt)
+{
+    byte[] saltBytes = Convert.FromBase64String(salt);
+    var pbkdf2Bytes = Rfc2898DeriveBytes.Pbkdf2(
+      enteredPassword,
+      saltBytes,
+      10_000,
+      HashAlgorithmName.SHA256,
+      32
+    );
+    var enteredHashedPassword = Convert.ToBase64String(pbkdf2Bytes);
+
+    return enteredHashedPassword == hashedPassword;
+}
